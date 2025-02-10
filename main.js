50a81362ed973b1922529e4ba8b8584d36ec6d66
zeratheuz <zeratheuz@gmail.com>
2025-02-10 17:25:48 -0300

    Add addPixels and removePixels functions

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
-for (let i = 0; i < 256; i++) {
-  const container = document.querySelector('.container')
-  const div = document.createElement('div')
-  div.setAttribute("class", 'pixel')
-  container.appendChild(div)
+let pixelsNumber = 256
+addPixels(pixelsNumber)
+const button = document.querySelector(".pixelsNumber")
+button.addEventListener("click", () => {
+  removePixels()
+  pixelsNumber = prompt("How many squares do you want?")
+  pixelsNumber *= pixelsNumber
+  addPixels(pixelsNumber)
+})
+
+function addPixels(num) {
+  for (let i = 0; i < num; i++) {
+    const container = document.querySelector('.container')
+    const div = document.createElement('div')
+    div.setAttribute("class", 'pixel')
+    container.appendChild(div)
+  }
+  const pixels = document.querySelectorAll('.pixel')
+  pixels.forEach((pixel) => {
+    pixel.addEventListener("mouseenter", (e) => {
+      pixel.setAttribute("style", "background-color: blueviolet")
+    })
+  })
 }
 
-const pixels = document.querySelectorAll('.pixel')
-pixels.forEach((pixel) => {
-  pixel.addEventListener("mouseenter", (e) => {
-    pixel.setAttribute("class", "color")
+function removePixels() {
+  const divs = document.querySelectorAll(".pixel")
+  divs.forEach((div) => {
+    div.remove()
   })
-} )
+}
