69b7ac078f99ac03c11384cad54b451fe198e400
zeratheuz <zeratheuz@gmail.com>
2025-02-11 15:45:16 -0300

    Refactor addPixels function

--- main.js
+++ main.js
@@ -10,26 +10,31 @@ button.addEventListener("click", () => {
 })
 
 function addPixels(num) {
+  const size = 64 / num
   for (let i = 0; i < (num * num); i++) {
     const container = document.querySelector('.container')
     const div = document.createElement('div')
     div.setAttribute("class", 'pixel')
-    const size = 64 / num
     div.setAttribute("style", `height: ${size}vmin; width: ${size}vmin;`)
     container.appendChild(div)
   }
   const pixels = document.querySelectorAll('.pixel')
   pixels.forEach((pixel) => {
     pixel.addEventListener("mouseenter", (e) => {
-      pixel.setAttribute("class", "color")
+      pixel.setAttribute("style", `
+        width: ${size}vmin;
+        height: ${size}vmin;
+        background-color: black 
+        `)
     })
   })
 }
 
 function removePixels() {
   const pixels = Array.from(document.querySelectorAll(".pixel"))
-  const colors = Array.from(document.querySelectorAll(".color"))
-  const divs = pixels.concat(colors) 
+  // const colors = Array.from(document.querySelectorAll(".color"))
+  // const divs = pixels.concat(colors) 
+  const divs = pixels 
   divs.forEach((div) => {
     div.remove()
   })
